09bd846fd8f530c1c7475baebd12848428fff3d2
Oleg Anohin <oanohin@outlook.com>
2021-09-15 10:18:48 +0400

    Start

---
+++ P12318_CodeC/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12318_CodeC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code C#");
+
+            Console.ReadKey();
+        }
+    }
+}
