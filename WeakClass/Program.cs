c9d828861b5d71e14328d9c91fbf16611b329ab2
Oleg Anohin <oanohin@outlook.com>
2022-01-27 15:35:36 +0400

    Add

---
+++ WeakClass/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeakClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person person = new Person();
+            new WeakClass().DoSomething();
+
+            Console.ReadKey();
+        }
+    }
+
+    class WeakClass
+    {
+        public string name = "Undefined";   // имя
+        public int age;                     // возраст
+
+        public WeakClass()
+        {
+            Console.WriteLine("Ctor");
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("DoSomething");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя: {name}  Возраст: {age}");
+        }
+    }
+}
